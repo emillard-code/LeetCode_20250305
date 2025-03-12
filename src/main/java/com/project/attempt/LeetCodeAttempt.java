d1e32aa80ab286dd6a995e282f863db3cd363606
Emerson Millard <emillard.code@gmail.com>
2025-03-12 12:00:00 -0700

    Added challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -4,6 +4,23 @@ public class LeetCodeAttempt {
 
     public static void main(String[] args) {
 
+        System.out.println(countTotalNumberOfColoredCells(1));
+        System.out.println(countTotalNumberOfColoredCells(2));
+
+    }
+
+    public static int countTotalNumberOfColoredCells(int n) {
+
+        int coloredCells = 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            coloredCells = coloredCells + (4 * (i - 1));
+
+        }
+
+        return coloredCells;
+
     }
 
 }
