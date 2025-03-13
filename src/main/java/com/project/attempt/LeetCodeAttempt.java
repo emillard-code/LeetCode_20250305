2530f40b5b056ab57757c7ed15b26f9f02d409b2
Emerson Millard <emillard.code@gmail.com>
2025-03-13 12:00:00 -0700

    Added comments to challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -9,16 +9,23 @@ public class LeetCodeAttempt {
 
     }
 
+    // This method count the total number of colored cells after int n minutes.
     public static int countTotalNumberOfColoredCells(int n) {
 
+        // During the first minute, we know the number of colored cells is 1.
         int coloredCells = 1;
 
+        // Each following minute, the number of colored cells should increase by the
+        // amount dictated by the following formula. The formula was obtained from
+        // looking at the outputs for each minute and deciphering the pattern between
+        // the output values of each successive minute.
         for (int i = 2; i <= n; i++) {
 
             coloredCells = coloredCells + (4 * (i - 1));
 
         }
 
+        // Finally, we return the number of colored cells.
         return coloredCells;
 
     }
