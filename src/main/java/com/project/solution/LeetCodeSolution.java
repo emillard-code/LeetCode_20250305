3f87b75a58eb579f16a7ff0587af79dfaf9c4658
Emerson Millard <emillard.code@gmail.com>
2025-03-14 12:00:00 -0700

    Added challenge solution.

--- src/main/java/com/project/solution/LeetCodeSolution.java
+++ src/main/java/com/project/solution/LeetCodeSolution.java
@@ -4,6 +4,25 @@ public class LeetCodeSolution {
 
     public static void main(String[] args) {
 
+        System.out.println(coloredCells(1));
+        System.out.println(coloredCells(2));
+
+    }
+
+    public static long coloredCells(int n) {
+
+        long numBlueCells = 1;
+        int addend = 4;
+
+        // Iterate n - 1 times
+        while (--n > 0) {
+            // Add and update current multiple of 4
+            numBlueCells += addend;
+            addend += 4;
+        }
+
+        return numBlueCells;
+
     }
 
 }
